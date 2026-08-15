9c985d6dc764a25118abca6ff121e0aa04050b13
Dylan Brown <20152200@tafe.wa.edu.au>
2026-08-15 20:51:46 +0800

    Added comments to help my understanding

--- Change.py
+++ Change.py
@@ -1,25 +1,32 @@
 def find_fewest_coins(coins, target):
+    # checks if target is negative
     if target < 0:
         raise ValueError("target can't be negative")
-         
-    coins_limit = target + 1
-    dp = [coins_limit] * (target + 1)
-    dp[0] = 0
 
-    for amount, number_of_coins in enumerate(dp):
+    # sets up an array with [coins_limit] instances of (target + 1)
+    coins_limit = target + 1
+    change_array = [coins_limit] * (target + 1)
+    change_array[0] = 0
+
+    # loops through array for no. of coins, 
+    # adds iterable and coin value to running total,
+    # exits loop when coin_limit is reached
+    for amount, number_of_coins in enumerate(change_array):
         if number_of_coins == coins_limit:
             continue
         for coin in coins:
             total = amount + coin
             if total <= target:
-                dp[total] = min(dp[total], number_of_coins + 1)
+                change_array[total] = min(change_array[total], number_of_coins + 1)
 
-    if dp[target] > target:
+    # If the array can't reach target value, raise error
+    if change_array[target] > target:
         raise ValueError("can't make target with given coins")
-    
+
+    # creates list of coins to reach target
     change = []
     for coin in coins:
-        while coin <= target and dp[target] == dp[target - coin] + 1:
+        while coin <= target and change_array[target] == change_array[target - coin] + 1:
             change.append(coin)
             target -= coin
 
